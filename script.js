04bdb203ada5cdf4cfe9e29891c8cf796c7387a4
AnisaHMohamed <anisahamohamed@gmail.com>
2022-08-17 18:04:49 -0400

    raven flapping at different intervals and speeds

--- script.js
+++ script.js
@@ -10,8 +10,11 @@ let lastTime = 0;
 let ravens = [];
 class Raven  {
     constructor(x){
-        this.width=100;
-        this.height= 50;
+        this.spriteWidth = 271;
+        this.spriteHeight = 194;
+        this.sizeModifier = Math.random() * 0.6 + 0.4
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier;
         this. x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() *  5 + 3;
@@ -19,16 +22,27 @@ class Raven  {
         this.markedForDeletion = false;
         this.image = new Image();
         this.image.src = './assets/raven.png';
-        this.spriteWidth = 271;
-        this.spriteHeight = 194;
+        this.frame = 0;
+        this.maxFrame = 4;
+        this.timeSinceFlap = 0;
+        this.flapInterval = Math.random() * 50 + 50;
+     
     }
-    update() {
-        this.x  -= this.directionX;
+    update(deltaTime) {
+        this.x -= this.directionX;
         if (this.x < 0 - this.width) this.markedForDeletion = true;
+        this.timeSinceFlap += deltaTime;
+        if (this.timeSinceFlap > this.flapInterval){
+            if (this.frame > this.maxFrame) this.frame = 0;
+            else this.frame++;
+            this.timeSinceFlap = 0;
+        }
+     
+
     }
     draw() {
         ctx.strokeRect(this.x,this.y,this.width,this.height);
-        ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
  }
 
@@ -41,7 +55,7 @@ class Raven  {
         ravens.push(new Raven());
         timeToNextRaven = 0;
     }
-    [...ravens].forEach((raven)=> raven.update());
+    [...ravens].forEach((raven)=> raven.update(deltaTime));
     [...ravens].forEach((raven)=> raven.draw());
     ravens = ravens.filter((raven)=> !raven.markedForDeletion)
     console.log(ravens);
