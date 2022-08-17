73326a6b84e36c59f136bd7705686ca34ba10133
AnisaHMohamed <anisahamohamed@gmail.com>
2022-08-17 17:41:05 -0400

    animated rectangles across screen and respawning

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+let timeToNextRaven = 0;
+let ravenInterval = 500;
+let lastTime = 0;
+
 let ravens = [];
- class Raven  {
+class Raven  {
     constructor(x){
         this.width=100;
         this.height= 50;
@@ -11,20 +16,32 @@ let ravens = [];
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() *  5 + 3;
         this.directionY = Math.random() *  5 - 2.5;
+        this.markedForDeletion = false;
+        this.image = new Image();
+        this.image.src = './assets/raven.png'
     }
     update() {
         this.x  -= this.directionX;
+        if (this.x < 0 - this.width) this.markedForDeletion = true;
     }
     draw() {
         ctx.fillRect(this.x,this.y,this.width,this.height);
     }
  }
 
- const raven = new Raven()
  const animate = (timestamp) => {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    raven.update();
-    raven.draw();
-    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let deltaTime = (timestamp - lastTime);
+    lastTime = timestamp;
+    timeToNextRaven += deltaTime;
+    if (timeToNextRaven > ravenInterval) {
+        ravens.push(new Raven());
+        timeToNextRaven = 0;
+    }
+    [...ravens].forEach((raven)=> raven.update());
+    [...ravens].forEach((raven)=> raven.draw());
+    ravens = ravens.filter((raven)=> !raven.markedForDeletion)
+    console.log(ravens);
+   requestAnimationFrame(animate);
  }
- animate()
+ animate(0)
